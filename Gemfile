cafee39bab590fb49f774fa7949957e2a661af5a
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-06-17 21:31:53 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1.0'
+
+gem 'pg'
+
+gem 'netzke-core', :git => 'git://github.com/nomadcoder/netzke-core.git'
+gem "netzke-basepack", :git => 'git://github.com/nomadcoder/netzke-basepack.git'
+
+# To use ActiveModel has_secure_password
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the app server
+gem 'unicorn'
+
+gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calculator::Application.config.secret_token = 'a2331b2f9ae207dfc5581f0236a153654a81f66344357a2f019e13cc2b02be943897f1d9a9f9455e0cf8b0d887b1844e63f529938718cb8e2d1e43df93dd5450'
